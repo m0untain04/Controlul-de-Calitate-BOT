870c7c7d487055119d2109139ba693c89e260201
m0untain04 <65610893+m0untain04@users.noreply.github.com>
2022-07-15 09:39:59 +0300

    Forgot to add some shit..

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,5 +1,10 @@
 
 
 bot.on('ready', async => {
-    console.log("Logged in!")
+    console.log("*  ===================================================  *")
+    console.log("           Controlul de Calitate has started             ")
+    console.log("*  ===================================================  *")
+    console.log("   Username:  "+bot.user.tag)
+    console.log("   Servers:    "+bot.guild.cache.size)
+    bot.user.setActivity(settings.activity);
 })
